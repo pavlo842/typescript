29190fffb565e046d7411b1580f5d7ce28e5b323
pavel <pishchalaups@belhard.com>
2020-05-14 09:25:01 +0300

    lesson 1 - basic type & compilation

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-var Q = 1;
+// const Q = 1;
+// string
+var str = 'string!'; // : string - указание типа переменной
+// number
+var num = 10;
+// boolean
+var isTrue = true;
+var isFalse = false;
+isTrue = false; // 0 указывать нельзя
+isFalse = true; // 1 указывать нельзя
+// other
+// 1-й способ
+var unknown = '1'; // any - в переменную можно записать любой тип данных
+unknown = 1;
+unknown = true;
+// 2-й способ
+var some = '1'; // | - перечисление конкретных типов которые будут записаны в переменную
+some = 1;
+some = false;
--- index.ts
+++ index.ts
@@ -1,1 +1,27 @@
-const Q = 1;
+// const Q = 1;
+
+// string
+let str: string = 'string!'; // : string - указание типа переменной
+
+// number
+let num: number = 10;
+
+// boolean
+let isTrue: boolean = true;
+let isFalse: boolean = false;
+
+isTrue = false; // 0 указывать нельзя
+isFalse = true; // 1 указывать нельзя
+
+// other
+// 1-й способ
+let unknown: any = '1'; // any - в переменную можно записать любой тип данных
+
+unknown = 1;
+unknown = true;
+
+// 2-й способ
+let some: string | number | boolean = '1'; // | - перечисление конкретных типов которые будут записаны в переменную
+
+some = 1;
+some = false;
