d9b8171973376d7f3889c5b433fbb15b726ae568
pavel <pishchalaups@belhard.com>
2020-05-15 20:18:28 +0300

    lesson 7 - classes

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 "use strict";
-// ENUM
-var Job;
-(function (Job) {
-    Job[Job["Frontend"] = 0] = "Frontend";
-    Job[Job["BackEnd"] = 50] = "BackEnd";
-    Job[Job["Desiner"] = 51] = "Desiner"; // 2. Если BackEnd = 50, то Desiner = 51
-})(Job || (Job = {}));
-var job = Job.BackEnd; // Job.BackEnd = 1. После задания константы на 50 - Job.BackEnd = 50 
-console.log(job); // по умолчанию - 1. После задания константы на 50 - Job.BackEnd = 50 
-// never - функции которые ничего не возвращают - выкидывают ошибку и дальше код работает
-function throwNewError(err) {
-    throw new Error(err);
+// Classes
+class User {
+    // public job: string; // второй вариант в constructor -> ()
+    constructor(name, job) {
+        this.job = job;
+        this.age = 35; // protected - доступны внутри класса и у наследников класса (class -> extends, super())
+        this.name = name;
+        // this.job = job; // второй вариант в ()
+    }
+    getAge() {
+        return this.age;
+    }
+    setTitle(title) {
+        console.log(this.isTeacher);
+        this.isTeacher = title;
+        console.log(this.isTeacher);
+    }
 }
-// null
-var newVar;
-newVar = null;
-var myNumber = 20;
-myNumber = null;
+const user = new User('PPS', 'FE');
+console.log(user);
+user.setTitle(true); // Таким образом получили доступ к переменной private isTeacher
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,22 +1,32 @@
-// ENUM
+// Classes
 
-enum Job {
-    Frontend, // 0
-    BackEnd = 50,  // по умолчанию - 1. 50 - самостоятельное задание константы
-    Desiner   // 2. Если BackEnd = 50, то Desiner = 51
+class User {
+
+    name: string; // public - default - доступно везде
+    private isTeacher: boolean | undefined; // private - доступна внутри класса
+    protected age: number = 35; // protected - доступны внутри класса и у наследников класса (class -> extends, super())
+    // public job: string; // второй вариант в constructor -> ()
+
+    constructor(
+            name: string,
+            public job: string
+        ) {
+        this.name = name;
+        // this.job = job; // второй вариант в ()
+    }
+
+    public getAge(): number { // public - default - доступно везде
+        return this.age;
+    }
+
+    public setTitle(title: boolean) {
+        console.log(this.isTeacher);
+        this.isTeacher = title;
+        console.log(this.isTeacher);
+    }
+
 }
 
-const job: Job = Job.BackEnd; // Job.BackEnd = 1. После задания константы на 50 - Job.BackEnd = 50 
-console.log(job); // по умолчанию - 1. После задания константы на 50 - Job.BackEnd = 50 
-
-// never - функции которые ничего не возвращают - выкидывают ошибку и дальше код работает
-function throwNewError(err: string): never {
-    throw new Error(err);
-}
-
-// null
-let newVar;
-newVar = null;
-
-let myNumber: number | null = 20;
-myNumber = null;
+const user = new User('PPS', 'FE');
+console.log(user);
+user.setTitle(true); // Таким образом получили доступ к переменной private isTeacher
