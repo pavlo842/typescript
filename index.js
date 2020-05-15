52cf7380afeb708a2c0c8c28fd8fbc5f746799da
pavel <pishchalaups@belhard.com>
2020-05-15 16:02:32 +0300

    lesson 6 - enum, never, null

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 "use strict";
-var user = {
-    name: 'PPS',
-    age: 35
-};
-function logUser(user) {
-    console.log(user.name + ' ' + user.age);
+// ENUM
+var Job;
+(function (Job) {
+    Job[Job["Frontend"] = 0] = "Frontend";
+    Job[Job["BackEnd"] = 50] = "BackEnd";
+    Job[Job["Desiner"] = 51] = "Desiner"; // 2. Если BackEnd = 50, то Desiner = 51
+})(Job || (Job = {}));
+var job = Job.BackEnd; // Job.BackEnd = 1. После задания константы на 50 - Job.BackEnd = 50 
+console.log(job); // по умолчанию - 1. После задания константы на 50 - Job.BackEnd = 50 
+// never - функции которые ничего не возвращают - выкидывают ошибку и дальше код работает
+function throwNewError(err) {
+    throw new Error(err);
 }
-logUser(user);
+// null
+var newVar;
+newVar = null;
+var myNumber = 20;
+myNumber = null;
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,15 +1,22 @@
-type User = {
-    name: string,
-    age: number,
+// ENUM
+
+enum Job {
+    Frontend, // 0
+    BackEnd = 50,  // по умолчанию - 1. 50 - самостоятельное задание константы
+    Desiner   // 2. Если BackEnd = 50, то Desiner = 51
 }
 
-const user: User = {
-    name: 'PPS',
-    age: 35
+const job: Job = Job.BackEnd; // Job.BackEnd = 1. После задания константы на 50 - Job.BackEnd = 50 
+console.log(job); // по умолчанию - 1. После задания константы на 50 - Job.BackEnd = 50 
+
+// never - функции которые ничего не возвращают - выкидывают ошибку и дальше код работает
+function throwNewError(err: string): never {
+    throw new Error(err);
 }
 
-function logUser(user: User): void {
-    console.log(user.name + ' ' + user.age);
-}
+// null
+let newVar;
+newVar = null;
 
-logUser(user);
+let myNumber: number | null = 20;
+myNumber = null;
