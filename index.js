1d9dc364af731f36fbd41bd877961411fb656570
pavel <pishchalaups@belhard.com>
2020-05-15 11:02:22 +0300

    lesson 4 - objects

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
-// Functions
-var myName = 'PPS';
-var myAge = 35;
-function getMyName() {
-    return myName;
-}
-console.log(getMyName());
-function getMyAge() {
-    return myAge;
-}
-console.log(getMyAge());
-function getMyAge1(age, prefix) {
-    if (prefix === void 0) { prefix = 'PPS-'; }
-    return prefix + age;
-}
-console.log(getMyAge1(36));
-// Тип функции которая ничего не возвращает
-function consoleLog(str) {
-    console.log(str);
-}
-consoleLog('Test string');
-// Синтаксис функций ES6
-var testFunc = function () { return 1 + 1; };
-var testFunc2 = function () {
-    return 1 + 1;
+// objects
+var user = {
+    name: 'PPS',
+    age: 35,
+    jobs: ['a', 'b'],
+    logName: function () {
+        console.log(this.name);
+    },
+    getJobs: function () {
+        return this.jobs;
+    }
 };
-// Синтаксис функций ES6
-var mySum; // если в последствии mySum станет функцией, то надо писать такой синтаксис
-function sum(num1, num2) {
-    return num1 + num2;
-}
-mySum = sum; // функция sum присваивается переменной mySum
-console.log(sum(5, 10));
-console.log(mySum(10, 15));
+var user2 = {
+    name: 'Max',
+    age: 30,
+    jobs: ['2'],
+    getJobs: function () {
+        return this.jobs;
+    }
+};
--- index.ts
+++ index.ts
@@ -1,48 +1,30 @@
-// Functions
+// objects
 
-const myName: string = 'PPS';
-const myAge: number = 35;
-
-function getMyName(): string { // : string - тип который будет возвращать функция
-    return myName;
-}
-console.log(getMyName());
-
-function getMyAge(): number {
-    return myAge;
-}
-console.log(getMyAge());
-
-
-
-function getMyAge1(age: number, prefix: string = 'PPS-'): string { // если параметр не передается, то надо задать по умолчанию и написать его в конце
-    return prefix + age;
-}
-console.log(getMyAge1(36));
-
-
-// Тип функции которая ничего не возвращает
-function consoleLog(str: string): void { // если функция без return то надо указать void
-    console.log(str);
-}
-consoleLog('Test string');
-
-
-// Синтаксис функций ES6
-const testFunc = () => 1 + 1;
-const testFunc2 = function() { // идентична testFunc
-    return 1 + 1;
-}
-// Синтаксис функций ES6
-
-let mySum: (a: number, b: number) => number; // если в последствии mySum станет функцией, то надо писать такой синтаксис
-
-function sum(num1: number, num2: number): number {
-    return num1 + num2;
+type User = {
+    name: string,
+    age: number,
+    getJobs: () => string[],
+    jobs: string[],
+    logName?: () => void // ? - указать для того чтобы небыло ошибки в объекте в котором нету данной функции
 }
 
-mySum = sum; // функция sum присваивается переменной mySum
+let user: User = {
+    name: 'PPS',
+    age: 35,
+    jobs: ['a', 'b'],
+    logName(): void {
+        console.log(this.name);
+    },
+    getJobs(): string[] {
+        return this.jobs;
+    }
+};
 
-console.log(sum(5, 10));
-console.log(mySum(10, 15));
-
+let user2: User = {
+    name: 'Max',
+    age: 30,
+    jobs: ['2'],
+    getJobs(): string[] {
+        return this.jobs;
+    }
+}
