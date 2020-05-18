97de9b433c2c4f1e2c8de255c16253a712690d69
pavel <pishchalaups@belhard.com>
2020-05-18 12:56:23 +0300

    lesson 9 - intefaces

--- index.js
+++ index.js
@@ -1,47 +1,24 @@
 "use strict";
-// inheritance Classes
+// interfaces
+// Наследоваться можно более чем от одного интрфейса.
+// После наследования необходимо реализовать в классе минимальное коичество полей и методов, предусмотренных интерфейсом
+// const user: IUser = {
+//     name: 'PPS',
+//     age: 35,
+//     logInfo(info) {
+//         console.log('Info ', info);
+//     }
+// }
+// Создание класса по интерфейсу
 class User {
-    constructor(name, job) {
-        this.name = name;
-        this.job = job;
-        this.age = 35; // protected - доступны внутри класса и у наследников класса (class -> extends, super())
-        // this.name = name;
-        // this.job = job; // второй вариант в ()
-    }
-    getAge() {
-        // меняется тип возвращаемых данных, то тут тоже надо менять тип
-        return this.age + '';
-    }
-}
-class PPS extends User {
-    constructor(job) {
-        super('PPS', job); // вызов родительского конструтора
-        this.age = 100; // после вызова родительского конструтора через super - есть доступ ко всем полям в родителе, кроме private
-    }
-    getAge() {
-        return 'Hello ' + this.age;
-    }
-}
-const user = new User('PPS', 'FE');
-const pps = new PPS('Frontend'); // создание экземпляра класса PPS
-// console.log(pps);
-// console.log(pps.getAge());
-// Создание Абстрактных классов - от которых нельзя делать экземпляры (объекты). Только наследование
-class Car {
     constructor() {
-        this.year = 2010;
+        this.name = 'pallo';
     }
-    getCarYear() {
-        return this.year;
-    }
-}
-class Audi extends Car {
     logInfo(info) {
         console.log(info);
     }
+    getYear() {
+        return 200;
+    }
 }
-const audi = new Audi();
-console.log(audi);
-audi.logInfo('info');
-console.log(audi.getCarYear());
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,63 +1,50 @@
-// inheritance Classes
+// interfaces
+// Наследоваться можно более чем от одного интрфейса.
+// После наследования необходимо реализовать в классе минимальное коичество полей и методов, предусмотренных интерфейсом
 
-class User {
-    private isTeacher: boolean | undefined; // private - доступна внутри класса
-    protected age: number = 35; // protected - доступны внутри класса и у наследников класса (class -> extends, super())
+// interface Ilength {
+//     length: number;
+// }
 
-    constructor(
-            public name: string,
-            public job: string
-        ) {
-        // this.name = name;
-        // this.job = job; // второй вариант в ()
-    }
+// function getLength(variable: Ilength): void {
+//     console.log(variable.length);
+// }
 
-    public getAge(): string { // public - default - доступно везде. Если в наследнике при переопределении метода
-                              // меняется тип возвращаемых данных, то тут тоже надо менять тип
-        return this.age + '';
-    }
+// const box = {
+//     name: 'PPS',
+//     length: 20
+// }
+
+// getLength(box);
+
+
+interface IUser {
+    name: string;
+    age?: number;
+    logInfo(info: string): void;
 }
 
-class PPS extends User { // наследование от класса User
-    constructor(job: string) {
-        super('PPS', job); // вызов родительского конструтора
-        this.age = 100; // после вызова родительского конструтора через super - есть доступ ко всем полям в родителе, кроме private
-    }
-
-    getAge(): string { // переопределение метода в родителе. String тут и д.б. в родителе string
-        return 'Hello ' + this.age;
-    }
-
+interface IGetYear {
+    getYear(): number;
 }
 
-const user = new User('PPS', 'FE');
-const pps = new PPS('Frontend'); // создание экземпляра класса PPS
-// console.log(pps);
-// console.log(pps.getAge());
+// const user: IUser = {
+//     name: 'PPS',
+//     age: 35,
+//     logInfo(info) {
+//         console.log('Info ', info);
+//     }
+// }
 
+// Создание класса по интерфейсу
 
-
-// Создание Абстрактных классов - от которых нельзя делать экземпляры (объекты). Только наследование
-abstract class Car {
-
-    model!: string;
-    year: number = 2010;
-
-    // создание абстрактного метода внутри абстрактного класса
-    abstract logInfo(info: string): void;
-
-    getCarYear(): number {
-        return this.year;
-    }
-}
-
-class Audi extends Car {
+class User implements IUser, IGetYear { // Наследование от двух интерфейсов
+    name: string = 'pallo';
     logInfo(info: string): void {
-        console.log(info);        
+        console.log(info);
+    }
+
+    getYear(): number {
+        return 200;
     }
 }
-
-const audi = new Audi();
-console.log(audi);
-audi.logInfo('info');
-console.log(audi.getCarYear());
