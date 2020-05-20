8e71aea4aa1e0f2b0b7e50add26288f457be9571
pavel <pishchalaups@belhard.com>
2020-05-20 14:13:04 +0300

    lesson 11 - decorator

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
 "use strict";
-// Generic
-// function getter(data: any) {
-//     return data;
+// Декораторы - специальная возможность typescript по добавлению метадаты в классы или функции
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+// Функция декоратор
+// function logger(constrFn: Function) {
+//     console.log(constrFn);
 // }
-// console.log(getter(100));
-// console.log(getter('PPS'));
-// ======================================================================
-// function on generic
-function genericGetter(data /*тип который передается в параметр*/) {
-    return data;
+// function shouldLog(flag: boolean): any {
+//     return flag ? logger : null;
+// }
+// @shouldLog(true) // Что-бы закрепить декоратор за классом - нужно перед классом поставить @<name-decorator>
+// class User {
+//     constructor(
+//         public name: string,
+//         public age: number,
+//     ) {
+//         console.log('Hello world');
+//     }
+// }
+// ===============================================================================
+// Функция декоратор
+function addShowAbility(constructorFn) {
+    // функция для добавления метода в класс
+    constructorFn.prototype.showHtml = function () {
+        const pre = document.createElement('pre'); // переменная pre - в которую запишется созданный html элемент
+        pre.innerHTML = JSON.stringify(this); // установление html разметки и запись в нее объекта user в формате JSON
+        document.body.appendChild(pre); // добавление дочернего элемента на страницу
+    };
 }
-console.log(genericGetter('PPS').length);
-console.log(genericGetter(100));
-let newGenericFunction /*переменная*/ = genericGetter /*функция которая записывается в переменную newGenericFunction*/;
-console.log(newGenericFunction('PPS').length);
-console.log(newGenericFunction(100));
-// =======================================================================
-// Generic with class
-class Multiply {
-    constructor(a, b) {
-        this.a = a;
-        this.b = b;
-    }
-    getResult() {
-        return +this.a * Number(this.b);
-    }
-}
-const mStr = new Multiply('10', '10');
-console.log(mStr.getResult());
-const mNum = new Multiply(10, 10);
-console.log(mNum.getResult());
+// Привязка к классу функции декоратора
+let User = /** @class */ (() => {
+    let User = class User {
+        constructor(name, age, job) {
+            this.name = name;
+            this.age = age;
+            this.job = job;
+        }
+    };
+    User = __decorate([
+        addShowAbility
+    ], User);
+    return User;
+})();
+let user = new User('PPS', 20, 'FE');
+console.log(user);
+user.showHtml(); // что-бы получить доступ к добавленному методу  нужно указать <any> перед объектом
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,42 +1,47 @@
-// Generic
-// function getter(data: any) {
-//     return data;
+// Декораторы - специальная возможность typescript по добавлению метадаты в классы или функции
+
+// Функция декоратор
+// function logger(constrFn: Function) {
+//     console.log(constrFn);
 // }
 
-// console.log(getter(100));
-// console.log(getter('PPS'));
+// function shouldLog(flag: boolean): any {
+//     return flag ? logger : null;
+// }
+
+// @shouldLog(true) // Что-бы закрепить декоратор за классом - нужно перед классом поставить @<name-decorator>
+// class User {
+//     constructor(
+//         public name: string,
+//         public age: number,
+//     ) {
+//         console.log('Hello world');
+//     }
+// }
 
 
-// ======================================================================
-// function on generic
-function genericGetter<T>/*тип который придет в функцию*/(data: T/*тип который передается в параметр*/): T/*тип который вернется из функции*/ {
-    return data;
-}
-
-console.log(genericGetter('PPS').length);
-console.log(genericGetter(100));
-
-let newGenericFunction/*переменная*/: <T>/*тип generic*/(data: T) => T/*функция возвращает тип Т*/ = genericGetter /*функция которая записывается в переменную newGenericFunction*/;
-
-console.log(newGenericFunction('PPS').length);
-console.log(newGenericFunction(100));
-
-
-// =======================================================================
-// Generic with class
-class Multiply<T extends number | string /*передаваемый тип наследуется от number | string*/> {
-    constructor(
-        private a: T,
-        private b: T,
-    ) {}
-
-    public getResult(): number {
-        return +this.a * Number(this.b);
+// ===============================================================================
+// Функция декоратор
+function addShowAbility(constructorFn: Function) {
+    // функция для добавления метода в класс
+    constructorFn.prototype.showHtml = function() {
+        const pre = document.createElement('pre'); // переменная pre - в которую запишется созданный html элемент
+        pre.innerHTML = JSON.stringify(this); // установление html разметки и запись в нее объекта user в формате JSON
+        document.body.appendChild(pre); // добавление дочернего элемента на страницу
     }
 }
 
-const mStr = new Multiply<string>('10', '10');
-console.log(mStr.getResult());
+// Привязка к классу функции декоратора
+@addShowAbility
+class User {
+    constructor(
+        public name: string,
+        public age: number,
+        public job: string,
+    ) {}
+}
 
-const mNum = new Multiply<number>(10, 10);
-console.log(mNum.getResult());
+let user = new User('PPS', 20, 'FE');
+console.log(user);
+
+(<any>user).showHtml(); // что-бы получить доступ к добавленному методу  нужно указать <any> перед объектом
